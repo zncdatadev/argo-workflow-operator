a60762842dbd95502676e15cb9e8d4c64c5d887f
bococ <Aliyum@88.com>
2023-11-10 18:56:16 +0800

    feat: add operator logic, and test local

---
+++ internal/controller/argoworkflow_controller.go
@@ -0,0 +1,169 @@
+/*
+Copyright 2023.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package controller
+
+import (
+	"context"
+	"github.com/go-logr/logr"
+	apimeta "k8s.io/apimachinery/pkg/api/meta"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/util/retry"
+
+	stackv1alpha1 "github.com/zncdata-labs/argo-workflow-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+// ArgoWorkFlowReconciler reconciles a ArgoWorkFlow object
+type ArgoWorkFlowReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+	Log    logr.Logger
+}
+
+//+kubebuilder:rbac:groups=stack.zncdata.net,resources=argoworkflows,verbs=get;list;watch;create;update;patch;delete
+//+kubebuilder:rbac:groups=stack.zncdata.net,resources=argoworkflows/status,verbs=get;update;patch
+//+kubebuilder:rbac:groups=stack.zncdata.net,resources=argoworkflows/finalizers,verbs=update
+// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=serviceaccounts,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=core,resources=pods,verbs=get;list;
+
+// +kubebuilder:rbac:groups="",resources=persistentvolumeclaims;persistentvolumeclaims/finalizers,verbs=get;create;update;delete
+// +kubebuilder:rbac:groups="",resources=pods;pods/exec,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=argoproj.io,resources=workflows;workflows/finalizers;workflowtasksets;workflowtasksets/finalizers;workflowartifactgctasks,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=argoproj.io,resources=workflowtemplates;workflowtemplates/finalizers,verbs=get;list;watch
+// +kubebuilder:rbac:groups=argoproj.io,resources=cronworkflows;cronworkflows/finalizers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups="",resources=events,verbs=create;patch
+// +kubebuilder:rbac:groups=argoproj.io,resources=workflowtaskresults,verbs=list;watch;deletecollection
+// +kubebuilder:rbac:groups="",resources=serviceaccounts,verbs=get;list
+// +kubebuilder:rbac:groups="policy",resources=poddisruptionbudgets,verbs=create;get;delete
+// +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=create
+// +kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,resourceNames=workflow-controller;workflow-controller-lease,verbs=get;list;watch;create;update;patch;delete
+
+// Reconcile is part of the main kubernetes reconciliation loop which aims to
+// move the current state of the cluster closer to the desired state.
+// TODO(user): Modify the Reconcile function to compare the state specified by
+// the ArgoWorkFlow object against the actual cluster state, and then
+// perform operations to make the cluster state reflect the state specified by
+// the user.
+//
+// For more details, check Reconcile and its Result here:
+// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.14.1/pkg/reconcile
+func (r *ArgoWorkFlowReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+
+	r.Log.Info("Reconciling ArgoWorkFlow")
+
+	argoWorkflow := &stackv1alpha1.ArgoWorkFlow{}
+
+	if err := r.Get(ctx, req.NamespacedName, argoWorkflow); err != nil {
+		if client.IgnoreNotFound(err) != nil {
+			r.Log.Error(err, "unable to fetch ArgoWorkFlow")
+			return ctrl.Result{}, err
+		}
+		r.Log.Info("ArgoWorkFlow resource not found. Ignoring since object must be deleted")
+		return ctrl.Result{}, nil
+	}
+
+	// Get the status condition, if it exists and its generation is not the
+	//same as the ArgoWorkFlow's generation, reset the status conditions
+	readCondition := apimeta.FindStatusCondition(argoWorkflow.Status.Conditions, stackv1alpha1.ConditionTypeProgressing)
+	if readCondition == nil || readCondition.ObservedGeneration != argoWorkflow.GetGeneration() {
+		argoWorkflow.InitStatusConditions()
+
+		if err := r.UpdateStatus(ctx, argoWorkflow); err != nil {
+			return ctrl.Result{}, err
+		}
+	}
+
+	r.Log.Info("ArgoWorkFlow found", "Name", argoWorkflow.Name)
+
+	if err := r.reconcileDeployment(ctx, argoWorkflow); err != nil {
+		r.Log.Error(err, "unable to reconcile Deployment")
+		return ctrl.Result{}, err
+	}
+
+	if err := r.reconcileService(ctx, argoWorkflow); err != nil {
+		r.Log.Error(err, "unable to reconcile Service")
+		return ctrl.Result{}, err
+	}
+
+	if err := r.reconcileServiceAccount(ctx, argoWorkflow); err != nil {
+		r.Log.Error(err, "unable to reconcile ServiceAccount")
+		return ctrl.Result{}, err
+	}
+
+	if err := r.reconcileClusterRoleBinding(ctx, argoWorkflow); err != nil {
+		r.Log.Error(err, "unable to reconcile ClusterRoleBinding")
+		return ctrl.Result{}, err
+	}
+
+	if err := r.reconcileConfigMap(ctx, argoWorkflow); err != nil {
+		r.Log.Error(err, "unable to reconcile ConfigMap")
+		return ctrl.Result{}, err
+	}
+
+	argoWorkflow.SetStatusCondition(metav1.Condition{
+		Type:               stackv1alpha1.ConditionTypeAvailable,
+		Status:             metav1.ConditionTrue,
+		Reason:             stackv1alpha1.ConditionReasonRunning,
+		Message:            "ArgoWorkFlow is running",
+		ObservedGeneration: argoWorkflow.GetGeneration(),
+	})
+
+	if err := r.UpdateStatus(ctx, argoWorkflow); err != nil {
+		return ctrl.Result{}, err
+	}
+
+	r.Log.Info("Successfully reconciled ArgoWorkFlow")
+	return ctrl.Result{}, nil
+}
+
+// UpdateStatus updates the status of the ArgoWorkFlow resource
+// https://stackoverflow.com/questions/76388004/k8s-controller-update-status-and-condition
+func (r *ArgoWorkFlowReconciler) UpdateStatus(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	retryErr := retry.RetryOnConflict(retry.DefaultRetry, func() error {
+		return r.Status().Update(ctx, instance)
+		//return r.Status().Patch(ctx, instance, client.MergeFrom(instance))
+	})
+
+	if retryErr != nil {
+		r.Log.Error(retryErr, "Failed to update vfm status after retries")
+		return retryErr
+	}
+
+	//if err := r.Get(ctx, key, latest); err != nil {
+	//	r.Log.Error(err, "Failed to get latest object")
+	//	return err
+	//}
+	//
+	//if err := r.Status().Patch(ctx, instance, client.MergeFrom(instance)); err != nil {
+	//	r.Log.Error(err, "Failed to patch object status")
+	//	return err
+	//}
+	r.Log.V(1).Info("Successfully patched object status")
+	return nil
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *ArgoWorkFlowReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	return ctrl.NewControllerManagedBy(mgr).
+		For(&stackv1alpha1.ArgoWorkFlow{}).
+		Complete(r)
+}
---
+++ internal/controller/handler.go
@@ -0,0 +1,275 @@
+package controller
+
+import (
+	"context"
+	stackv1alpha1 "github.com/zncdata-labs/argo-workflow-operator/api/v1alpha1"
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	rbacv1 "k8s.io/api/rbac/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+)
+
+func (r *ArgoWorkFlowReconciler) makeService(instance *stackv1alpha1.ArgoWorkFlow, schema *runtime.Scheme) *corev1.Service {
+	labels := instance.GetLabels()
+	svc := &corev1.Service{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:        instance.Name,
+			Namespace:   instance.Namespace,
+			Labels:      labels,
+			Annotations: instance.Spec.Service.Annotations,
+		},
+		Spec: corev1.ServiceSpec{
+			Ports: []corev1.ServicePort{
+				{
+					Port:     instance.Spec.Service.Port,
+					Name:     "http",
+					Protocol: "TCP",
+				},
+			},
+			Selector: labels,
+			Type:     instance.Spec.Service.Type,
+		},
+	}
+	err := ctrl.SetControllerReference(instance, svc, schema)
+	if err != nil {
+		r.Log.Error(err, "Failed to set controller reference for service")
+		return nil
+	}
+	return svc
+}
+
+func (r *ArgoWorkFlowReconciler) reconcileService(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	obj := r.makeService(instance, r.Scheme)
+	if obj == nil {
+		return nil
+	}
+
+	if err := CreateOrUpdate(ctx, r.Client, obj); err != nil {
+		r.Log.Error(err, "Failed to create or update service")
+		return err
+	}
+	return nil
+}
+
+func (r *ArgoWorkFlowReconciler) makeDeployment(instance *stackv1alpha1.ArgoWorkFlow, schema *runtime.Scheme) *appsv1.Deployment {
+	labels := instance.GetLabels()
+	envVars := []corev1.EnvVar{}
+
+	envVars = append(envVars, corev1.EnvVar{
+		Name: "ARGO_NAMESPACE",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				APIVersion: "v1",
+				FieldPath:  "metadata.namespace",
+			},
+		},
+	})
+	envVars = append(envVars, corev1.EnvVar{
+		Name: "LEADER_ELECTION_IDENTITY",
+		ValueFrom: &corev1.EnvVarSource{
+			FieldRef: &corev1.ObjectFieldSelector{
+				APIVersion: "v1",
+				FieldPath:  "metadata.name",
+			},
+		},
+	})
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.Name,
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &instance.Spec.Replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: labels,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: labels,
+				},
+				Spec: corev1.PodSpec{
+					ServiceAccountName: instance.GetNameWithSuffix("-controller"),
+					SecurityContext:    instance.Spec.SecurityContext,
+					Containers: []corev1.Container{
+						{
+							Name:            instance.Name,
+							Image:           instance.Spec.Image.Repository + ":" + instance.Spec.Image.Tag,
+							ImagePullPolicy: instance.Spec.Image.PullPolicy,
+							Args: []string{
+								"--configmap",
+								"argo-wrokflows-argo-workflows-controller",
+								"--executor-image",
+								"docker.io/bitnami/argo-workflow-exec:3.5.0-debian-11-r0",
+								"--executor-image-pull-policy",
+								"IfNotPresent",
+								"--loglevel",
+								"info",
+								"--gloglevel",
+								"0",
+								"--workflow-workers",
+								"32",
+							},
+							Env:       envVars,
+							Resources: *instance.Spec.Resources,
+							Ports: []corev1.ContainerPort{
+								{
+									ContainerPort: 18080,
+									Name:          "http",
+									Protocol:      "TCP",
+								},
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := ctrl.SetControllerReference(instance, dep, schema)
+	if err != nil {
+		r.Log.Error(err, "Failed to set controller reference for deployment")
+		return nil
+	}
+	return dep
+}
+
+func (r *ArgoWorkFlowReconciler) updateStatusConditionWithDeployment(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow, status metav1.ConditionStatus, message string) error {
+	instance.SetStatusCondition(metav1.Condition{
+		Type:               stackv1alpha1.ConditionTypeProgressing,
+		Status:             status,
+		Reason:             stackv1alpha1.ConditionReasonReconcileDeployment,
+		Message:            message,
+		ObservedGeneration: instance.GetGeneration(),
+		LastTransitionTime: metav1.Now(),
+	})
+
+	if err := r.UpdateStatus(ctx, instance); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *ArgoWorkFlowReconciler) reconcileDeployment(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	obj := r.makeDeployment(instance, r.Scheme)
+	if obj == nil {
+		return nil
+	}
+	if err := CreateOrUpdate(ctx, r.Client, obj); err != nil {
+		logger.Error(err, "Failed to create or update deployment")
+		return err
+	}
+
+	return nil
+}
+
+func (r *ArgoWorkFlowReconciler) makeServiceAccount(instance *stackv1alpha1.ArgoWorkFlow, schema *runtime.Scheme) *corev1.ServiceAccount {
+	labels := instance.GetLabels()
+	satoken := true
+	sa := &corev1.ServiceAccount{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.GetNameWithSuffix("-controller"),
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		AutomountServiceAccountToken: &satoken,
+	}
+	err := ctrl.SetControllerReference(instance, sa, schema)
+	if err != nil {
+		r.Log.Error(err, "Failed to set controller reference for ServiceAccount")
+		return nil
+	}
+	return sa
+}
+
+func (r *ArgoWorkFlowReconciler) reconcileServiceAccount(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	obj := r.makeServiceAccount(instance, r.Scheme)
+	if obj == nil {
+		return nil
+	}
+
+	if err := CreateOrUpdate(ctx, r.Client, obj); err != nil {
+		r.Log.Error(err, "Failed to create or update ServiceAccount")
+		return err
+	}
+	return nil
+}
+
+func (r *ArgoWorkFlowReconciler) makeClusterRoleBinding(instance *stackv1alpha1.ArgoWorkFlow, schema *runtime.Scheme) *rbacv1.ClusterRoleBinding {
+	labels := instance.GetLabels()
+	subject := rbacv1.Subject{
+		Kind:      "ServiceAccount",
+		Name:      instance.GetNameWithSuffix("-controller"),
+		Namespace: instance.Namespace,
+	}
+	subjects := []rbacv1.Subject{subject}
+	crbd := &rbacv1.ClusterRoleBinding{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.GetNameWithSuffix("-controller"),
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		RoleRef: rbacv1.RoleRef{
+			APIGroup: "rbac.authorization.k8s.io",
+			Kind:     "ClusterRole",
+			Name:     "manager-role",
+		},
+		Subjects: subjects,
+	}
+	err := ctrl.SetControllerReference(instance, crbd, schema)
+	if err != nil {
+		r.Log.Error(err, "Failed to set controller reference for ClusterRoleBinding")
+		return nil
+	}
+	return crbd
+}
+
+func (r *ArgoWorkFlowReconciler) reconcileClusterRoleBinding(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	obj := r.makeClusterRoleBinding(instance, r.Scheme)
+	if obj == nil {
+		return nil
+	}
+
+	if err := CreateOrUpdate(ctx, r.Client, obj); err != nil {
+		r.Log.Error(err, "Failed to create or update ServiceAccount")
+		return err
+	}
+	return nil
+}
+
+func (r *ArgoWorkFlowReconciler) makeConfigMap(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow, schema *runtime.Scheme) *corev1.ConfigMap {
+	labels := instance.GetLabels()
+
+	configMap := &corev1.ConfigMap{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      instance.GetNameWithSuffix("-controller"),
+			Namespace: instance.Namespace,
+			Labels:    labels,
+		},
+		Data: map[string]string{
+			"config": "parallelism: \nnamespaceParallelism: \nexecutor: \n  resources: \n    limits: {} \n    requests: {} \n",
+		},
+	}
+
+	err := ctrl.SetControllerReference(instance, configMap, schema)
+	if err != nil {
+		r.Log.Error(err, "Failed to set controller reference for configmap")
+		return nil
+	}
+	return configMap
+}
+
+func (r *ArgoWorkFlowReconciler) reconcileConfigMap(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow) error {
+	obj := r.makeConfigMap(ctx, instance, r.Scheme)
+	if obj == nil {
+		return nil
+	}
+
+	if err := CreateOrUpdate(ctx, r.Client, obj); err != nil {
+		r.Log.Error(err, "Failed to create or update service")
+		return err
+	}
+	return nil
+}
