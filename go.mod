7d49ea4f7c20ed05c8bff3f816f9f20d19db5d97
steven <lwpk110@163.com>
2024-01-29 20:56:14 +0800

    refactor(roleGroup): add the concept of role-group for all resources (#12)
    * refactor(roleGroup): add the concept of role-group for all resources
    
    * style: rm unused code with golang-lint
    
    ---------
    
    Co-authored-by: luwei <wei.lu@zncdata.com>

--- go.mod
+++ go.mod
@@ -1,16 +1,17 @@
 module github.com/zncdata-labs/argo-workflow-operator
 
-go 1.19
+go 1.21
 
 require (
 	github.com/cisco-open/k8s-objectmatcher v1.9.0
 	github.com/go-logr/logr v1.3.0
-	github.com/onsi/ginkgo/v2 v2.11.0
-	github.com/onsi/gomega v1.27.10
-	k8s.io/api v0.28.3
-	k8s.io/apimachinery v0.28.3
-	k8s.io/client-go v0.28.3
-	k8s.io/kubectl v0.28.3
+	github.com/onsi/ginkgo/v2 v2.13.0
+	github.com/onsi/gomega v1.29.0
+	github.com/zncdata-labs/operator-go v0.2.0
+	k8s.io/api v0.29.0
+	k8s.io/apimachinery v0.29.0
+	k8s.io/client-go v0.29.0
+	k8s.io/kubectl v0.29.0
 	sigs.k8s.io/controller-runtime v0.16.3
 )
 
@@ -67,7 +68,7 @@ require (
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 	k8s.io/apiextensions-apiserver v0.28.3 // indirect
-	k8s.io/component-base v0.28.3 // indirect
+	k8s.io/component-base v0.29.0 // indirect
 	k8s.io/klog/v2 v2.110.1 // indirect
 	k8s.io/kube-openapi v0.0.0-20231010175941-2dd684a91f00 // indirect
 	k8s.io/utils v0.0.0-20230726121419-3b25d923346b // indirect
---
+++ internal/controller/resource_reconciler.go
@@ -0,0 +1,45 @@
+package controller
+
+import (
+	"context"
+
+	stackv1alpha1 "github.com/zncdata-labs/argo-workflow-operator/api/v1alpha1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+)
+
+func (r *ArgoWorkFlowReconciler) extractResources(instance *stackv1alpha1.ArgoWorkFlow, ctx context.Context,
+	roleGroupExtractor func(*stackv1alpha1.ArgoWorkFlow, context.Context, string, *stackv1alpha1.RoleConfigSpec,
+		*runtime.Scheme) (client.Object, error)) ([]client.Object, error) {
+	var resources []client.Object
+	if instance.Spec.RoleGroups != nil {
+		for roleGroupName, roleGroup := range instance.Spec.RoleGroups {
+			rsc, err := roleGroupExtractor(instance, ctx, roleGroupName, roleGroup, r.Scheme)
+			if err != nil {
+				return nil, err
+			}
+			resources = append(resources, rsc)
+		}
+	}
+	return resources, nil
+}
+func (r *ArgoWorkFlowReconciler) createOrUpdateResource(ctx context.Context, instance *stackv1alpha1.ArgoWorkFlow,
+	roleGroupExtractor func(*stackv1alpha1.ArgoWorkFlow, context.Context, string, *stackv1alpha1.RoleConfigSpec,
+		*runtime.Scheme) (client.Object, error)) error {
+	resources, err := r.extractResources(instance, ctx, roleGroupExtractor)
+	if err != nil {
+		return err
+	}
+
+	for _, rsc := range resources {
+		if rsc == nil {
+			continue
+		}
+
+		if err := CreateOrUpdate(ctx, r.Client, rsc); err != nil {
+			r.Log.Error(err, "Failed to create or update Resource", "resource", rsc)
+			return err
+		}
+	}
+	return nil
+}
